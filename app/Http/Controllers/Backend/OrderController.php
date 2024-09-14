41df54ec15297d335eee70c70cdcd564e4026d19
thoriqbani <tjrsgamers@gmail.com>
2024-09-14 20:28:21 +0700

    views coupon, order, report

--- app/Http/Controllers/Backend/OrderController.php
+++ app/Http/Controllers/Backend/OrderController.php
@@ -11,7 +11,7 @@ use App\Models\Course_goal;
 use App\Models\CourseSection;
 use App\Models\CourseLecture;
 use Intervention\Image\Facades\Image;
-use Illuminate\Support\Facades\Auth; 
+use Illuminate\Support\Facades\Auth;
 use Carbon\Carbon;
 use Gloudemans\Shoppingcart\Facades\Cart;
 use App\Models\Coupon;
@@ -20,6 +20,7 @@ use App\Models\Payment;
 use App\Models\Order;
 use Barryvdh\DomPDF\Facade\Pdf;
 use App\Models\Question;
+use Illuminate\Support\Facades\DB;
 
 class OrderController extends Controller
 {
@@ -28,8 +29,8 @@ class OrderController extends Controller
         $payment = Payment::where('status','pending')->orderBy('id','DESC')->get();
         return view('admin.backend.orders.pending_orders',compact('payment'));
 
-    } // End Method 
-    
+    } // End Method
+
 
     public function AdminOrderDetails($payment_id){
 
@@ -38,7 +39,7 @@ class OrderController extends Controller
 
         return view('admin.backend.orders.admin_order_details',compact('payment','orderItem'));
 
-    }// End Method 
+    }// End Method
 
 
     public function PendingToConfirm($payment_id){
@@ -48,10 +49,10 @@ class OrderController extends Controller
             'message' => 'Order Confrim Successfully',
             'alert-type' => 'success'
         );
-        return redirect()->route('admin.confirm.order')->with($notification);  
+        return redirect()->route('admin.confirm.order')->with($notification);
 
 
-    }// End Method 
+    }// End Method
 
 
     public function AdminConfirmOrder(){
@@ -59,21 +60,21 @@ class OrderController extends Controller
         $payment = Payment::where('status','confirm')->orderBy('id','DESC')->get();
         return view('admin.backend.orders.confirm_orders',compact('payment'));
 
-    } // End Method 
+    } // End Method
 
     public function InstructorAllOrder(){
 
         $id = Auth::user()->id;
 
-        $latestOrderItem = Order::where('instructor_id',$id)->select('payment_id', \DB::raw('MAX(id) as max_id'))->groupBy('payment_id');
+        $latestOrderItem = Order::where('instructor_id',$id)->select('payment_id', DB::raw('MAX(id) as max_id'))->groupBy('payment_id');
 
         $orderItem = Order::joinSub($latestOrderItem, 'latest_order', function($join) {
             $join->on('orders.id', '=', 'latest_order.max_id');
         })->orderBy('latest_order.max_id','DESC')->get();
-        
+
         return view('instructor.orders.all_orders',compact('orderItem'));
 
-    }// End Method 
+    }// End Method
 
 
     public function InstructorOrderDetails($payment_id){
@@ -83,7 +84,7 @@ class OrderController extends Controller
 
         return view('instructor.orders.instructor_order_details',compact('payment','orderItem'));
 
-    }// End Method 
+    }// End Method
 
 
     public function InstructorOrderInvoice($payment_id){
@@ -97,21 +98,21 @@ class OrderController extends Controller
         ]);
         return $pdf->download('invoice.pdf');
 
-    }// End Method 
+    }// End Method
 
 
     public function MyCourse(){
         $id = Auth::user()->id;
 
-        $latestOrders = Order::where('user_id',$id)->select('course_id', \DB::raw('MAX(id) as max_id'))->groupBy('course_id');
+        $latestOrders = Order::where('user_id',$id)->select('course_id', DB::raw('MAX(id) as max_id'))->groupBy('course_id');
 
         $mycourse = Order::joinSub($latestOrders, 'latest_order', function($join) {
             $join->on('orders.id', '=', 'latest_order.max_id');
         })->orderBy('latest_order.max_id','DESC')->get();
-        
+
         return view('frontend.mycourse.my_all_course',compact('mycourse'));
 
-    }// End Method 
+    }// End Method
 
 
     public function CourseView($course_id){
@@ -125,9 +126,9 @@ class OrderController extends Controller
         return view('frontend.mycourse.course_view',compact('course','section','allquestion'));
 
 
-    }// End Method 
+    }// End Method
 
 
 
 
-} 
+}
--- resources/views/admin/backend/coupon/coupon_all.blade.php
+++ resources/views/admin/backend/coupon/coupon_all.blade.php
@@ -1,9 +1,95 @@
 @extends('admin.admin_dashboard')
 @section('admin')
 
-<div class="page-content">
+<div class="content-body">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-xl-12">
+                @if (Session::has('error'))
+                <div class="alert alert-danger solid alert-dismissible fade show">
+                    <svg viewBox="0 0 24 24" width="24 " height="24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" class="me-2"><polygon points="7.86 2 16.14 2 22 7.86 22 16.14 16.14 22 7.86 22 2 16.14 2 7.86 7.86 2"></polygon><line x1="15" y1="9" x2="9" y2="15"></line><line x1="9" y1="9" x2="15" y2="15"></line></svg>
+                    <strong>Error!</strong> {{ Session::get('error') }}
+                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="btn-close">
+                    </button>
+                </div>
+                @endif
+                @if(Session::has('success'))
+                <div class="alert alert-success solid alert-dismissible fade show">
+                    <svg viewBox="0 0 24 24" width="24" height="24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" class="me-2"><polyline points="9 11 12 14 22 4"></polyline><path d="M21 12v7a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h11"></path></svg>
+                    <strong>Success!</strong> {{ Session::get('success') }}
+                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="btn-close">
+                    </button>
+                </div>
+                @endif
+            </div>
+            <div class="col-xl-12">
+                <div class="row">
+                    <div class="col-xl-12">
+                        <div class="page-title flex-wrap">
+                            <form action="" method="GET" class="input-group search-area mb-md-0 mb-3" >
+                                <input type="search" class="form-control" name="search" placeholder="Search here...">
+                                <span class="input-group-text">
+                                    <button type="submit">
+                                        <svg width="15" height="15" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                            <path d="M17.5605 15.4395L13.7527 11.6317C14.5395 10.446 15 9.02625 15 7.5C15 3.3645 11.6355 0 7.5 0C3.3645 0 0 3.3645 0 7.5C0 11.6355 3.3645 15 7.5 15C9.02625 15 10.446 14.5395 11.6317 13.7527L15.4395 17.5605C16.0245 18.1462 16.9755 18.1462 17.5605 17.5605C18.1462 16.9747 18.1462 16.0252 17.5605 15.4395V15.4395ZM2.25 7.5C2.25 4.605 4.605 2.25 7.5 2.25C10.395 2.25 12.75 4.605 12.75 7.5C12.75 10.395 10.395 12.75 7.5 12.75C4.605 12.75 2.25 10.395 2.25 7.5V7.5Z" fill="#01A3FF"/>
+                                        </svg>
+                                    </button>
+                                </span>
+                            </form>
+                            <div>
+                                <a href="{{ route('admin.add.coupon') }}" class="btn btn-primary px-5">Add Coupon</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xl-12">
+                        <h5>All Coupon</h5>
+                        <div class="table-responsive full-data">
+                            <table id="example" class="table table-striped table-bordered" style="width:100%">
+                                <thead>
+                                    <tr>
+                                        <th>Sl</th>
+                                        <th>Coupon Name</th>
+                                        <th>Coupon Discount</th>
+                                        <th>Coupon Validity</th>
+                                        <th>Coupon Status </th>
+                                        <th>Action</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @foreach ($coupon as $key=> $item)
+                                    <tr>
+                                        <td>{{ $key+1 }}</td>
+                                        <td> {{ $item->coupon_name }} </td>
+                                        <td>{{ $item->coupon_discount }}%</td>
+                                        <td> {{ Carbon\Carbon::parse($item->coupon_validity)->format('D, d F Y')  }} </td>
+                                        <td>
+                                            @if ($item->coupon_validity >= Carbon\Carbon::now()->format('Y-m-d'))
+                                            <span class="badge bg-success">Valid</span>
+                                            @else
+                                            <span class="badge bg-danger">Invalid</span>
+                                            @endif
+                                        </td>
+                                        <td>
+                                            <a href="{{ route('admin.edit.coupon',$item->id) }}" class="btn btn-info px-5">Edit </a>
+                                            <a href="{{ route('admin.delete.coupon',$item->id) }}" class="btn btn-danger px-5" id="delete">Delete </a>
+                                        </td>
+                                    </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
+                            {{-- {{ $dataPacketFull->links() }} --}}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+@endsection
+{{-- <div class="page-content">
     <!--breadcrumb-->
-    <div class="page-breadcrumb d-none d-sm-flex align-items-center mb-3"> 
+    <div class="page-breadcrumb d-none d-sm-flex align-items-center mb-3">
         <div class="ps-3">
             <nav aria-label="breadcrumb">
                 <ol class="breadcrumb mb-0 p-0">
@@ -15,12 +101,12 @@
         </div>
         <div class="ms-auto">
             <div class="btn-group">
-           <a href="{{ route('admin.add.coupon') }}" class="btn btn-primary px-5">Add Coupon </a>  
+           <a href="{{ route('admin.add.coupon') }}" class="btn btn-primary px-5">Add Coupon </a>
             </div>
         </div>
     </div>
     <!--end breadcrumb-->
-  
+
     <div class="card">
         <div class="card-body">
             <div class="table-responsive">
@@ -29,48 +115,39 @@
                         <tr>
                             <th>Sl</th>
                             <th>Coupon Name  </th>
-                            <th>Coupon Discount</th> 
-                            <th>Coupon Validity</th> 
-                            <th>Coupon Status </th> 
+                            <th>Coupon Discount</th>
+                            <th>Coupon Validity</th>
+                            <th>Coupon Status </th>
                             <th>Action</th>
                         </tr>
                     </thead>
                     <tbody>
-                       
-@foreach ($coupon as $key=> $item) 
+
+@foreach ($coupon as $key=> $item)
 <tr>
     <td>{{ $key+1 }}</td>
     <td> {{ $item->coupon_name }} </td>
-    <td>{{ $item->coupon_discount }}%</td> 
+    <td>{{ $item->coupon_discount }}%</td>
     <td> {{ Carbon\Carbon::parse($item->coupon_validity)->format('D, d F Y')  }} </td>
-    <td>  
+    <td>
  @if ($item->coupon_validity >= Carbon\Carbon::now()->format('Y-m-d'))
- <span class="badge bg-success">Valid</span>   
- @else 
+ <span class="badge bg-success">Valid</span>
+ @else
  <span class="badge bg-danger">Invalid</span>
  @endif
     </td>
 
     <td>
-<a href="{{ route('admin.edit.coupon',$item->id) }}" class="btn btn-info px-5">Edit </a>   
-<a href="{{ route('admin.delete.coupon',$item->id) }}" class="btn btn-danger px-5" id="delete">Delete </a>                    
+<a href="{{ route('admin.edit.coupon',$item->id) }}" class="btn btn-info px-5">Edit </a>
+<a href="{{ route('admin.delete.coupon',$item->id) }}" class="btn btn-danger px-5" id="delete">Delete </a>
     </td>
 </tr>
 @endforeach
-                         
+
                     </tbody>
-                     
+
                 </table>
             </div>
         </div>
     </div>
-
-
-   
-   
-</div>
- 
-
-
-
-@endsection
+</div> --}}
